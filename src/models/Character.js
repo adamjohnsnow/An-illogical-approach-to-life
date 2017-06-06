fc2113ba534fdf1c9fced0cd98cc80f81269db2e
Adam Snow <adamjohnsnow@icloud.com>
2017-06-06 11:27:20 +0100

    add userid and avatar to character model

--- src/models/Character.js
+++ src/models/Character.js
@@ -7,10 +7,12 @@ mongoose.connect(testDB)
 
 var characterSchema = new Schema({
   name: { type: String, unique : true, required : true },
+  userId: String,
   xp: { type: Number, default: 0 },
   attack: { type: Number, default: 0 },
   defence: { type: Number, default: 0 },
-  level: { type: Number, default: 1 }
+  level: { type: Number, default: 1 },
+  avatar: String
 });
 
 var Character = mongoose.model('Character', characterSchema);
